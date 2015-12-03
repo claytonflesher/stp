11a1857b8ace4e73f963a8c140de46100c4ae7ea
Calvyn82 <claytonflesher@gmail.com>
2015-12-02 18:08:10 -0600

    Test coverage for patient_therapist_relationship model

--- spec/models/patient_therapist_relationship_spec.rb
+++ spec/models/patient_therapist_relationship_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe PatientTherapistRelationship, type: :model do
+  it "creates associations between therapists and patients" do
+    create(:patient)
+    patient   = Patient.last
+    create(:therapist)
+    therapist = Therapist.last
+    association = PatientTherapistRelationship.new(
+      patient_id: patient.id,
+      therapist_id: therapist.id
+    )
+    association.save
+
+    expect(association).to be_valid
+    expect(patient.therapists.first).to eq(therapist)
+    expect(therapist.patients.first).to eq(patient)
+  end
 end
