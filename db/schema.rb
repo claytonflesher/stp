a7d1dac353f180b3f35149a07a7be7bf0d1e6908
Calvyn82 <claytonflesher@gmail.com>
2015-12-16 11:11:27 -0600

    Created admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216161922) do
+ActiveRecord::Schema.define(version: 20151216164609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "username",            null: false
+    t.string   "password_digest",     null: false
+    t.string   "email",               null: false
+    t.string   "first_name",          null: false
+    t.string   "last_name",           null: false
+    t.string   "phone",               null: false
+    t.string   "address",             null: false
+    t.string   "city",                null: false
+    t.string   "state",               null: false
+    t.string   "country",             null: false
+    t.string   "zipcode",             null: false
+    t.string   "practice",            null: false
+    t.integer  "years_experience",    null: false
+    t.string   "qualifications",      null: false
+    t.string   "website"
+    t.string   "gender",              null: false
+    t.string   "religion",            null: false
+    t.string   "former_religion"
+    t.string   "licenses",            null: false
+    t.string   "main_license",        null: false
+    t.boolean  "distance_counseling", null: false
+    t.string   "languages"
+    t.string   "purpose",             null: false
+    t.string   "description"
+    t.string   "approach"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
 
   create_table "patient_therapist_relationships", force: :cascade do |t|
     t.integer  "patient_id"
