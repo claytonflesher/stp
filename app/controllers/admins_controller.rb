db164e89ade7ce134b317a55669cd06038432b76
Calvyn82 <claytonflesher@gmail.com>
2015-12-16 12:34:49 -0600

    Added index to therapists controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,10 @@
 class AdminsController < ApplicationController
   before_filter :ensure_admin
 
+  def index
+    @admins = Therapist.find_by admin: true
+  end
+
   def show
     @admin = Therapist.find(session[:therapist_id])
   end
