910ff87800ffedf651237a331df49fc285e2acf0
sivins <sean.ivins@yahoo.com>
2015-12-17 07:02:38 -0600

    Destroyed the scaffold, forgot about the gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :messages
   get    "home"                            => "static_pages#home"
   get    "about"                           => "static_pages#about"
   get    "contact"                         => "static_pages#contact"
