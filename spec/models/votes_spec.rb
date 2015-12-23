8ef825838c555cd425c51a0009e93264093cb9cc
Calvyn82 <claytonflesher@gmail.com>
2015-12-23 16:57:48 -0600

    Added test coverage to Vote model

--- spec/models/votes_spec.rb
+++ spec/models/votes_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Vote, type: :model do
   it { should belong_to :voter }
   it { should belong_to :votee }
+  it { should validate_presence_of(:voter_id) }
+  it { should validate_uniqueness_of(:voter_id).scoped_to(:votee_id) }
+  it { should validate_presence_of(:votee_id) }
 end
