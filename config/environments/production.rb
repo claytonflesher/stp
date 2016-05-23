07e333b3bd31cdb9cf3ce6faec1d66bc66ac286b
Calvyn82 <claytonflesher@gmail.com>
2016-05-22 21:23:30 -0500

    sendgrid integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: ENV['HOST']}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['EMAIL_USER_NAME'],
-    password:             ENV['EMAIL_PASSWORD'],
-    authentication:       'plain',
+    domain:               'heroku.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    authentication:       :plain,
     enable_starttls_auto: true }
 end
