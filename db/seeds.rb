c9a89ceed57fbc5cca108134e8acbc2e6c48de3d
Daniel Ashcraft <ashcraftbookkeeping@gmail.com>
2016-07-26 15:53:20 +0000

    Add seeds.rb for testuser and test password (therapist only at this point)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ Therapist.create!(
+     :username => 'testuser1',
+     :password =>'Welcome1',
+     :password_confirmation => 'Welcome1',
+     :first_name => 'Daniel',
+     :last_name => 'Ashcraft',
+     :address => '2236 carlton way',
+     :city => 'Oklahoma City',
+     :state => 'Oklahoma',
+     :country => 'United States',
+     :practice => 'Therapy',
+     :years_experience => 10,
+     :qualifications => 'Too many to list',
+     :gender => 'Male',
+     :religion => 'Atheist',
+     :licenses => 'Oklahoma state Therapist License #001',
+     :main_license => 'Oklahoma state Therapist License #001',
+     :distance_counseling => 'true',
+     :purpose => 'To help people',
+     :geo_address => '2236 calront way',
+     :super_admin => 'true',
+     :application_status => 'active',
+     :verified_at => Time.now.to_datetime,
+     :verification_token => 'abcdefghijklmnop',
+     :zipcode => 73120,
+     :email => 'testemail1@email.com',
+     :phone => '4054461822'
+     )
+   
