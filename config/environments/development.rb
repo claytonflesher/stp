64215e0b464eff7d7760e928ae809b7c40ba302e
sivins <sean.ivins@yahoo.com>
2016-08-28 14:27:10 -0500

    Configured development environment to use localhost fakesmtp server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Development smtp settings
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV["EMAIL_SMTP_ADDRESS"],
+    port: ENV["EMAIL_PORT"],
+    #user_name: ENV["EMAIL_USER_NAME"],
+    #password: ENV["EMAIL_PASSWORD"],
+    #authentication: 'plain',
+    #enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
